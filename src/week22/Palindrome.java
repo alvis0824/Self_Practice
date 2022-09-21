c2fcac71dad145e3e82485fc27fd489789c9b96d
Dilbar <dibux9@gmail.com>
2022-09-20 23:14:53 -0400

    commit

--- src/week22/Palindrome.java
+++ src/week22/Palindrome.java
@@ -4,17 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Palindrome {
 
     public static void main(String[] args) {
 
-        String str = "Do geese see God?";
+        String str = "A brown fox jumping over";
         System.out.println(isPalindrome(str));
     }
 
     private static boolean isPalindrome(String str) {
+        List<Character> chars = str.toLowerCase().chars()
+                .mapToObj(i -> (char)i)
+                .filter(Character::isLetter)
+                .collect(Collectors.toList());
 
-        return true;
+        for (int i = 0, j = chars.size()-1; i < chars.size(); i++,j--) {
+            if(chars.get(i).equals(chars.get(j))) return true;
+        }
+        return false;
     }
 }
